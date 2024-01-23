29544d6b3091d8648f21b803a2e411ebf54b0d53
PravalikaDasari <152262748+PravalikaDasari@users.noreply.github.com>
2024-01-23 12:33:17 +0530

    Delete virtualcare_registrationAndLogin directory

--- virtualcare_registrationAndLogin/virtualcare_registrationAndLogin/src/main/java/com/virtualcare_registartionAndLogin/bean/AdminBean.java
+++
@@ -1,6 +0,0 @@
-package com.virtualcare_registartionAndLogin.bean;
-
-public class AdminBean {
-	private Long admin_id;
-	
-}
--- virtualcare_registrationAndLogin/virtualcare_registrationAndLogin/src/main/java/com/virtualcare_registartionAndLogin/service/DoctorService.java
+++
@@ -1,8 +0,0 @@
-package com.virtualcare_registartionAndLogin.service;
-
-import com.virtualcare_registartionAndLogin.bean.DoctorBean;
-
-public interface DoctorService {
-	public void save(DoctorBean doctorBean);
-	public String loginDoctor(Long doctor_id,String doctor_password);
-}
