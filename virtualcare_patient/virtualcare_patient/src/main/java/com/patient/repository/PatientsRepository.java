c8461c7c6533d56c712b8da895b9cd5cdb9885bb
PravalikaDasari <152262748+PravalikaDasari@users.noreply.github.com>
2024-01-25 21:49:44 +0530

    Delete virtualcare_patient/virtualcare_patient directory

--- virtualcare_patient/virtualcare_patient/src/main/java/com/patient/repository/PatientsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.patient.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.patient.bean.Patients;
-
-public interface PatientsRepository  extends JpaRepository<Patients, Long>  {
-
-}
