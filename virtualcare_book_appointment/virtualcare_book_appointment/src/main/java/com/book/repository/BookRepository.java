1faeeacc43d1d2dd9593d5ea0f70aa99908b76b1
PravalikaDasari <152262748+PravalikaDasari@users.noreply.github.com>
2024-01-25 21:49:08 +0530

    Delete virtualcare_book_appointment/virtualcare_book_appointment directory

--- virtualcare_book_appointment/virtualcare_book_appointment/src/main/java/com/book/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.book.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.book.bean.BookAppointment;
-
-public interface BookRepository  extends JpaRepository<BookAppointment, Long>  {
-
-}
