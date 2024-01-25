2ed0b935cb4f199cc1a4560e7865cdb8c2e27561
PravalikaDasari <152262748+PravalikaDasari@users.noreply.github.com>
2024-01-25 21:49:32 +0530

    Delete virtualcare_location/virtualcare_location directory

--- virtualcare_location/virtualcare_location/src/main/java/com/virtualcare/repository/BranchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.virtualcare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.virtualcare.entity.BranchEntity;
-
-public interface BranchRepository extends JpaRepository<BranchEntity, String>{
-
-}
--- virtualcare_location/virtualcare_location/src/main/java/com/virtualcare/repository/StateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.virtualcare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.virtualcare.bean.StateBean;
-
-public interface StateRepository extends JpaRepository<StateBean, String> {
-
-}
